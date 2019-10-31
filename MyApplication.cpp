6c4daf8134c9cf227a2556db14d45ae4d08be8dc
Aayush Kapoor <xeoneux@icloud.com>
2019-10-31 11:35:14 +0530

    Get started with a basic window and gl info

---
+++ MyApplication.cpp
@@ -0,0 +1,38 @@
+#include <Corrade/Utility/DebugStl.h>
+#include <Magnum/GL/Context.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/GL/Version.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Platform/Sdl2Application.h>
+
+using namespace Magnum;
+
+class MyApplication : public Platform::Application {
+public:
+  explicit MyApplication(const Arguments &arguments);
+
+private:
+  void drawEvent() override;
+};
+
+MyApplication::MyApplication(const Arguments &arguments)
+    : Platform::Application{arguments} {
+  using namespace Math::Literals;
+
+  GL::Renderer::setClearColor(0xa5c9ea_rgbf);
+
+  Debug{} << "Hello! This application is running on"
+          << GL::Context::current().version() << "using"
+          << GL::Context::current().rendererString();
+}
+
+void MyApplication::drawEvent() {
+  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+
+  // TODO: Add your drawing code here
+
+  swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(MyApplication);
